5146bfc696abe086ee44cbe9e0935d30bb26fc5d
“Adebola07” <07zamani@gmail.com>
2022-09-07 18:04:50 +0000

    completed

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t adebola07/flaskapp .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run -it -p 8000:80 adebola07/flaskapp
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="adebola07/flaskapp:v1.0.0"
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag  adebola07/flaskapp $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
